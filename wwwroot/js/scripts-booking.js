34641ab9e639b2868b379e2db743fdea0290456c
William Eastbury <wieastbu@microsoft.com>
2017-03-15 15:34:39 +0000

    tweaking stackoverflow code.

--- wwwroot/js/scripts-booking.js
+++ wwwroot/js/scripts-booking.js
@@ -1,20 +1,3 @@
-// From StackOverflow 
-// http://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
-
-(function($) {
-    $.QueryString = (function(a) {
-        if (a == "") return {};
-        var b = {};
-        for (var i = 0; i < a.length; ++i) {
-            var p = a[i].split('=', 2);
-            if (p.length != 2) continue;
-            b[p[0]] = decodeURIComponent(p[1].replace(/\+/g, " "));
-        }
-        return b;
-    })(window.location.search.substr(1).split('&'))
-})(jQuery);
-
-// End of stack overflow copied code.
 // IIFE to protect the global scope and internal variables.
 (function() {
 
