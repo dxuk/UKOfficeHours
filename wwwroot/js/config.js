c89913be21ec044aa6bb901cbf585fe4861d58c5
William Eastbury <wieastbu@microsoft.com>
2017-03-27 13:49:35 +0100

    Update of settings layout

--- wwwroot/js/config.js
+++ wwwroot/js/config.js
@@ -2,9 +2,10 @@ var instance = 'https://login.microsoftonline.com/'; // Azure AD logon endpoint
 var dateform = "DD/MM/YYYY"; // UK locale
 var timeform = "HH:mm"; // 12h UK time
 
+var namesplitter = "officehours";
 // Figure out what the current URI is and where to get our config from.
 var serverprefixaddress = window.location.href.split("/")[2].split(".")[0];
-var prefix = serverprefixaddress.split("officehours")[0]
+var prefix = serverprefixaddress.split(namesplitter)[0]
 var resource = 'https://' + prefix + 'ukohfn.azurewebsites.net';
 var endpoint = 'https://' + prefix + '-ukofficehours.azurewebsites.net/';
 var rootfnsite = resource + "/";
