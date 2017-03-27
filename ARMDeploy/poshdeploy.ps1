3ee8399f736064e2db890cc29c87be3ab2bba7ef
William Eastbury <wieastbu@microsoft.com>
2017-03-27 11:53:28 +0100

    Finalising Parameters and Azure AD Deploy - Note Fn App is broken in this checkin

--- ARMDeploy/poshdeploy.ps1
+++ ARMDeploy/poshdeploy.ps1
@@ -17,10 +17,9 @@ param
 
 )
 
-
-Write-Host "Script Running - deploying rg $deployname" -ForegroundColor yellow
+Write-Host "Script Running - deploying environment $deployname to rg $rg in subscription $sub" -ForegroundColor yellow
 $templatefile = ((Get-Item -Path ".\" -Verbose).FullName + "\armdeploy\ohbooking.json")
-$tempparamfile = ((Get-Item -Path ".\" -Verbose).FullName + "\armdeploy\ohbooking" + $deployname + "params.json")
+$tempparamfile = ((Get-Item -Path ".\" -Verbose).FullName + "\armdeploy\ohbookingparams.json")
 $temploc = "test.output.txt"
 Add-Type -A System.IO.Compression.FileSystem
 
@@ -35,7 +34,11 @@ if ($currentapp -ne $null)
 else
 {
     Write-Host "App URI not found ... Creating AD Application $display" -ForegroundColor yellow
-    $currentapp = New-AzureRmADApplication -DisplayName $display -HomePage $siteroot -IdentifierUris $siteroot 
+    $currentapp = New-AzureRmADApplication -DisplayName $display -HomePage $siteroot -IdentifierUris $siteroot -ReplyUrls @($siteroot)
+    
+    # We could fire the Set-AzureADApplication cmdlet from the AzureAD module here to add OAuth Implicit flow support here
+    # but we will add this manually to the manifest at the end of the 1st deployment.
+     
     Write-Host "Created App: " $currentapp.ApplicationId
 }
 
@@ -49,7 +52,7 @@ Write-Host "Resource Group Deployment Running" -ForegroundColor yellow
 # Trigger the resource group deployment #
 Select-AzureRmSubscription -SubscriptionName $sub
 New-AzureRmResourceGroup -Name $rg -Location $loc -Force
-New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templatefile -Force -TemplateParameterFile $tempparamfile -AzureAD_ClientID $clientid
+New-AzureRmResourceGroupDeployment -ResourceGroupName $rg -TemplateFile $templatefile -Force -TemplateParameterFile $tempparamfile -prefix $deployname -AzureAD_TenantURI $tenanturi -AzureAD_ClientID $clientid
 
 Write-Host "Resource Group Deployment Complete" -ForegroundColor Green
 Write-Host "Function App Deployment Running" -ForegroundColor yellow
@@ -78,6 +81,25 @@ $sitename = ($deployname + "-ukofficehours")
 $base64AuthInfo2 = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username2, $password2)))
 $apiwb = "https://$sitename.scm.azurewebsites.net/api/zip/site/wwwroot/"
 Invoke-RestMethod -Uri $apiwb -Headers @{Authorization=("Basic {0}" -f $base64AuthInfo2)} -Method PUT -InFile ((Get-Item -Path ".\" -Verbose).FullName + "\deploy\web.zip")
+
+Write-Host "Deploying Storage Account Tables" -ForegroundColor Green
+
+# Create the tables inside the storage accounts
+
+##############
+# bookingslot
+# isv
+##############
+
+# Define the storage account and context.
+$StorageAccountName = $deployname + "ukohstoragedata"
+$Ctx = New-AzureStorageContext $StorageAccountName -StorageAccountKey (Get-AzureRmStorageAccountKey -Name ($deployname + "ukohstoragedata") -ResourceGroupName ohdv)[0].Value
+
+#Create the tables
+New-AzureStorageTable –Name "isv" -Context $Ctx -ErrorAction ignore
+New-AzureStorageTable –Name "bookingslot" –Context $Ctx -ErrorAction ignore
+
+$Ctx = $null
 
 Write-Host "All Deployments Complete, $sitename online" -ForegroundColor Green
 
