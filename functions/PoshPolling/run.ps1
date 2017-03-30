b0e6d302a342e72413f789c6a631be420c64729a
Mike Ormond <mormond@microsoft.com>
2017-03-30 16:23:45 +0100

    Route posh polling to localhost for debug

--- functions/PoshPolling/run.ps1
+++ functions/PoshPolling/run.ps1
@@ -1,2 +1,7 @@
 Write-Output "Keep-alive ping:$(get-date)";
-$resp = Invoke-WebRequest -uri "https://" + $env:APPSETTING_WEBSITE_SITE_NAME + "/api/GetConfig" -UseBasicParsing
+
+if ($env:APPSETTING_WEBSITE_SITE_NAME) {
+    $resp = Invoke-WebRequest -uri "https://" + $env:APPSETTING_WEBSITE_SITE_NAME + "/api/GetConfig" -UseBasicParsing
+} else {
+    $resp = Invoke-WebRequest -uri "http://localhost:7071/api/GetConfig" -UseBasicParsing
+}
