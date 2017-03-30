358fe3fb6473c3612a9a69c679f64f091b3c061d
Mike Ormond <mormond@microsoft.com>
2017-03-30 18:23:09 +0100

    Revert polling to use config

--- functions/PoshPolling/run.ps1
+++ functions/PoshPolling/run.ps1
@@ -1,7 +1,2 @@
 Write-Output "Keep-alive ping:$(get-date)";
-
-if ($env:APPSETTING_WEBSITE_SITE_NAME) {
-    $resp = Invoke-WebRequest -uri "https://" + $env:APPSETTING_WEBSITE_SITE_NAME + "/api/GetConfig" -UseBasicParsing
-} else {
-    $resp = Invoke-WebRequest -uri "http://localhost:7071/api/GetConfig" -UseBasicParsing
-}
+$resp = Invoke-WebRequest -uri "https://" + $env:APPSETTING_WEBSITE_SITE_NAME + "/api/GetConfig" -UseBasicParsing
