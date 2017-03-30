21e2db855562f75274e31baf8dd691ab200cb433
Mike Ormond <mormond@microsoft.com>
2017-03-30 16:00:56 +0100

    Refactor and remove dependency on config file

--- functions/configure-local-debug.ps1
+++ functions/configure-local-debug.ps1
@@ -1,34 +1,52 @@
 
 #
-#
+# PowerShell script to enable local debugging of website and functions
+# Launches 
+# Azure Storage Emulator - local emulator for Azure Storage
+# IIS Express - web server to host the wwwroot website
+# Func.exe - host for the Azure functions
 #
 
 Param(
-    # Parameter help description
-    [bool]$invokeCommands = $false
+    # Set to false to run but not execute the commands for debugging
+    [bool]$InvokeCommands = $true
 )
+
+$env:AzureWebJobsStorage = "DefaultEndpointsProtocol=http;AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;"
 
 $invocationPath = Split-Path $MyInvocation.MyCommand.Path
 $configFilepath = "$invocationPath\local-debug-config.json"
 
-Push-Location
-Set-Location -Path "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\Storage Emulator"
-if ($invokeCommands) { & ".\AzureStorageEmulator.exe" status }
-Pop-Location
+function LaunchStorageEmulator() {
+    Push-Location
+    Set-Location -Path "${env:ProgramFiles(x86)}\Microsoft SDKs\Azure\Storage Emulator"
+    if ($InvokeCommands) { 
+        $result = & ".\AzureStorageEmulator.exe" status 
+    }
+    Pop-Location
+}
 
-$iisExpress = "${env:ProgramFiles(x86)}\IIS Express\iisexpress.exe"
-# IIS Express not happy with other methods used to generate path - this works
-$physicalPath = (Get-Item -Path "$invocationPath\..\wwwroot").FullName
-$command = "`"$iisExpress`" /path:`"$physicalPath`""
-if ($invokeCommands) { cmd /c start cmd /k $command }
+function LaunchIISExpress() {
+    $iisExpress = "${env:ProgramFiles(x86)}\IIS Express\iisexpress.exe"
+    # IIS Express not happy with other methods used to generate path - this works
+    $physicalPath = (Get-Item -Path "$invocationPath\..\wwwroot").FullName
+    $command = "`"$iisExpress`" /path:`"$physicalPath`""
+    if ($InvokeCommands) {
+        cmd /c start cmd /k $command 
+    }
+}
 
+function LaunchFuncExe() {
+    Push-Location
+    Set-Location $invocationPath
+    if ($InvokeCommands) {
+        cmd /c start cmd /k "func host start --cors http://localhost:8080" 
+    }
+    Pop-Location
+}
+
+LaunchStorageEmulator
+LaunchIISExpress
 Start-Sleep -Seconds 3
-
-$config = ConvertFrom-Json "$(get-content $configFilepath)"
-$env:AzureWebJobsStorageConnection = $config.AzureWebJobsStorage
-
-Push-Location
-Set-Location $invocationPath
-if ($invokeCommands) { cmd /c start cmd /k "func host start" }
-Pop-Location
+LaunchFuncExe
 
